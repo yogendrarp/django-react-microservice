188b256b3a521f4c45a4290cd9cf00c8f8848cfa
Yogendra <ryogendrap@gmail.com>
2023-07-26 23:35:47 -0700

    Docker files

---
+++ admin-control/products/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from .views import ProductViewSet
+
+urlpatterns = [
+    path('products', ProductViewSet.as_view({
+        'get':'list',
+        'post':'create'
+    })),
+    path('products/<str:pk>', ProductViewSet.as_view({
+        'get':'retrieve',
+        'post':'update',
+        'delete':'destroy'
+    })),
+]
---
+++ admin-control/products/views.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        #/api/products
+        pass
+
+    def create(self, request):#/api/products
+        pass
+
+    def retrieve(self, request, pk=None):#/api/products/<str:id>
+        pass
+
+    def update(self, request, pk=None):#/api/products/<str:id>
+        pass
+
+    def destroy (self, request, pk=None):#/api/products/<str:id>
+        pass
