08b82fbf89e5b39bfac9a25f667542e9204aabd9
Yogendra <ryogendrap@gmail.com>
2023-07-29 22:48:26 -0700

    rabbit mq integration

--- admin-control/consumer.py
+++ admin-control/consumer.py
@@ -1,5 +1,5 @@
 import pika
-
+import json
 params = pika.URLParameters("amqps://sgknscuf:WUbB_Bl3EDLCOCfEVs1wCQDxbOw7lhkp@gull.rmq.cloudamqp.com/sgknscuf")
 
 connection = pika.BlockingConnection(params)
@@ -9,10 +9,15 @@ channel = connection.channel()
 channel.queue_declare(queue='admin')
 
 def callback(ch, method, properties, body):
-    print("Received in admin")
-    print(body)
+    data = json.loads(body)
+    print(data)
 
-channel.basic_consume(queue='admin', on_message_callback=callback)
+    if properties.content_type == "product_created ":
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+
+channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
 print('Started consuming')
 channel.start_consuming()
 channel.close()
--- flask-app/consumer.py
+++ flask-app/consumer.py
@@ -1,5 +1,6 @@
 import pika
-
+import json
+from main import Product, db
 params = pika.URLParameters("amqps://sgknscuf:WUbB_Bl3EDLCOCfEVs1wCQDxbOw7lhkp@gull.rmq.cloudamqp.com/sgknscuf")
 
 connection = pika.BlockingConnection(params)
@@ -9,10 +10,24 @@ channel = connection.channel()
 channel.queue_declare(queue='main')
 
 def callback(ch, method, properties, body):
-    print("Received in main")
-    print(body)
+    data = json.loads(body)
+    print(data)
 
-channel.basic_consume(queue='main', on_message_callback=callback)
+    if properties.content_type == "product_created ":
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+    elif properties.content_type == "product_updated":
+        product = Product.query.get(data["id"])
+        product.title = data["title"]
+        product.image = data["image"]
+        db.session.commit()
+    elif properties.content_type == "product_deleted":
+        product = product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+
+channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
 print('Started consuming')
 channel.start_consuming()
 channel.close()
